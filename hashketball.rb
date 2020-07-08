645a06b9df55b65bcb40cd4d0253454dfc417ad1
Jake Mills <jake.mills.05@gmail.com>
2020-07-08 23:19:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,7 +233,7 @@ def most_points_scored
   most_player = ""
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-      if attribute = :players
+      if attribute == :players
         data.each do |player|
           if player[:points] > most_points
             most_points = player[:points]
