e6dfd45d35cae031f7d88161546df60fb0d6f778
Jake Mills <jake.mills.05@gmail.com>
2020-07-09 02:38:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -208,8 +208,17 @@ def player_stats(player_name)
 end
 
 def big_shoe_rebounds
-  player_with_longest_name
-  binding.pry
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name|
+          if name[:player_name].length > longest_name.length
+            longest_name = name[:player_name]
+          end
+        end
+      end
+    end
+  end
 end
 
 # !!!!!!BONUS QUESTIONS!!!!!!
