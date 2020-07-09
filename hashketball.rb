b497067003c769d5cc640e40090f95e7e8e0a9b6
Jake Mills <jake.mills.05@gmail.com>
2020-07-09 03:03:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -223,6 +223,7 @@ def big_shoe_rebounds
           end
           data.all? do |stole|
             stole[:player_name] == longest_name
+            stole[:steals]
             binding.pry
           end
         end
